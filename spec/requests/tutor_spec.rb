634145f27163206fb54344824383cb60178db345
sarang-selldo <sarang.barshikar@sell.do>
2022-12-22 20:34:44 +0530

    Fix tutor spec

--- spec/requests/tutor_spec.rb
+++ spec/requests/tutor_spec.rb
@@ -5,31 +5,33 @@ require 'rails_helper'
 RSpec.describe Tutor, type: :request do
   let(:course){ create(:course) }
 
-  context 'when tutor params are invalid' do
-    context 'when tutor name is not present' do
-      it 'responds with appropriate error message' do
-        course
-        post '/tutors', params: { name: '', course_id: course.id }
-        response_body = JSON.parse(response.body)
-        expect(response_body).to eq(
-          {
-            "message" => "Failed to create tutor", 
-            "error_message" => "Name can't be blank"
-          }
-        )
+  describe '#create' do
+    context 'when tutor params are invalid' do
+      context 'when tutor name is not present' do
+        it 'responds with appropriate error message' do
+          course
+          post '/tutors', params: { name: '', course_id: course.id }
+          response_body = JSON.parse(response.body)
+          expect(response_body).to eq(
+            {
+              "message" => "Failed to create tutor",
+              "error_message" => "Name can't be blank"
+            }
+          )
+        end
       end
-    end
 
-    context 'when course is not present' do
-      it 'responds with appropriate error message' do
-        post '/tutors', params: { name: '', course_id: 10 }
-        response_body = JSON.parse(response.body)
-        expect(response_body).to eq(
-          {
-            "error_message" => "Invalid Course", 
-            "message" => ""
-          }
-        )
+      context 'when course is not present' do
+        it 'responds with appropriate error message' do
+          post '/tutors', params: { name: '', course_id: 10 }
+          response_body = JSON.parse(response.body)
+          expect(response_body).to eq(
+            {
+              "error_message" => "Invalid Course",
+              "message" => ""
+            }
+          )
+        end
       end
     end
 
